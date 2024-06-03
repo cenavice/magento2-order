690377decfa7e5b1f341301002064ee4642652a3
cenavice <avicenaarifin@gmail.com>
2024-06-03 14:36:36 +0000

    add cli to create bonus order

--- Console/Command/CreateBonusOrder.php
+++ Console/Command/CreateBonusOrder.php
@@ -9,12 +9,48 @@ use Symfony\Component\Console\Output\OutputInterface;
 class CreateBonusOrder extends Command
 {
     /**
+     * @var \Magento\Framework\ObjectManagerInterface
+     */
+    protected $_objectmanager;
+
+    /**
+     * @var \Cenavice\Order\Helper\Data
+     */
+    protected $helperData;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Framework\ObjectManagerInterface $objectmanager
+     * @param \Cenavice\Order\Helper\Data $helperData
+     */
+    public function __construct(
+        \Magento\Framework\ObjectManagerInterface $objectmanager,
+        \Cenavice\Order\Helper\Data $helperData
+    ) {
+        // $this->objectmanager = $objectManager;
+        $this->_objectmanager = $objectmanager;
+        $this->helperData = $helperData;
+        parent::__construct();
+    }
+
+    /**
      * @inheritDoc
      */
     protected function configure()
     {
         $this->setName('cenavice:order:create-bonus-order');
         $this->setDescription('This is my console command.');
+        // $this->setDefinition(
+        //     [
+        //         new InputArgument(
+        //             'main_order_entity_id',
+        //             InputArgument::REQUIRED,
+        //             'Main Order Entity ID'
+        //         ),
+        //     ]
+        // );  
+
 
         parent::configure();
     }
@@ -27,7 +63,25 @@ class CreateBonusOrder extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('<info>Success Message.</info>');
-        $output->writeln('<error>An error encountered.</error>');
+        $state = $this->_objectmanager->get('Magento\Framework\App\State');
+        try {
+           $state->setAreaCode('adminhtml');            
+        } catch (\Magento\Framework\Exception\LocalizedException $e) {
+           
+        }
+
+        $output->writeln('Creating bonus order ...');  
+        try {
+            $orderIncrementId = '000000005';
+            $qty = 3;
+            $this->helperData->createBonusOrder($orderIncrementId, $qty);
+            $output->writeln('<info>Success Message.</info>');
+        } catch (\Throwable $th) {
+            $output->writeln('<error>An error encountered.</error>');
+            $output->writeln('<error>'.__($th->getMessage()).'</error>');
+            return 0;
+        }
+
+        return 1;
     }
 }
